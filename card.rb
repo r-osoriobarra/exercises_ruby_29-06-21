53e69ffb6b71b93e6da76853b67899130217d492
rodrigo <rob.developer@protonmail.com>
2021-06-30 22:34:45 -0400

    add: class Card

--- card.rb
+++ card.rb
@@ -1,3 +1,16 @@
 class Card
+    attr_accessor :number, :kind
 
+    def initialize(number, kind)
+        if (["C","D","E","T"].include? kind) && ((1..13).include? number)
+            @number = number
+            @kind = kind
+        else
+            raise "number o kind no es un argumento válido" 
+        end
+    end
 end
+
+card_deck = Array.new
+5.times {|i| Card.new(2,'E')
+
